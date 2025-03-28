5a67549007da5af8fdeb49eb00cf59360d1db0df
lcaohoanq <hoangclw@gmail.com>
2025-03-28 16:20:20 +0700

    chore: resolve

--- claimrequest-fe-team1-dev/src/components/NotFound/NotFound.tsx
+++ claimrequest-fe-team1-dev/src/components/NotFound/NotFound.tsx
@@ -1,7 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
-import Button from "antd/lib/Button";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -11,9 +7,9 @@ const NotFound: React.FC = () => {
     <div className="flex min-h-screen items-center justify-center">
       <h1 className="text-4xl font-bold">404 - Not Found</h1>
       <p className="mt-4">The content you are looking for does not exist.</p>
-      <Button type="primary" className="mt-4" onClick={() => navigate("/")}>
+      <button className="mt-4" onClick={() => navigate("/")}>
         Go to Home
-      </Button>
+      </button>
     </div>
   );
 };
